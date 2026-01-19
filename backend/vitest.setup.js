3cfd1f9746f17bee6417fa4cbe8a26c8e7f785a4
Luca Nagy Abonyi <luca.nagy.abonyi@gmail.com>
2026-01-19 23:21:17 +0100

    Update vitest.setup

--- backend/vitest.setup.js
+++ backend/vitest.setup.js
@@ -2,14 +2,6 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 
-beforeAll(async () => {
-  if (!process.env.MONGO_URI) {
-    throw new Error("MONGO_URI not defined");
-  }
-
-  await mongoose.connect(process.env.MONGO_URI);
-});
-
 afterAll(async () => {
   await mongoose.connection.close();
 });
