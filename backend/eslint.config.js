1c62a221ef8072b8057276ec45d868cf50ade5b3
Luca Nagy Abonyi <luca.nagy.abonyi@gmail.com>
2026-01-19 21:45:52 +0100

    #15 Set up ESlint for CI pipeline

---
+++ backend/eslint.config.js
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+    },
+  },
+]);
