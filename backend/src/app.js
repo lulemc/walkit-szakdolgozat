e8f99dde5228a8ef0b6e0a0e3a5775f5fef3f718
lulemc <39914535+lulemc@users.noreply.github.com>
2026-01-20 21:41:54 +0100

    #9  Implement login and registration endpoints with middleware
    * #9 Create routes and userController to crate registration
    
    * Add test file and install vitest coverage

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import userRoutes from "./routes/userRoutes.js";
+import authRoutes from "./routes/auth.js";
 
 const app = express();
 
@@ -8,11 +10,13 @@ app.use(
     origin: ["http://localhost:3000", "http://localhost:8081"], // Whitelist the domains you want to allow
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  })
+  }),
 );
 
 app.use(express.json());
 
 // API routes
+app.use("/api/users", userRoutes);
+app.use("/auth", authRoutes);
 
 export default app;
--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,5 +1,9 @@
+import express from "express";
 import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
 
+const router = express.Router();
 // Middleware to verify token
 const authMiddleware = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1]; // Bearer <token>
@@ -14,5 +18,28 @@ const authMiddleware = (req, res, next) => {
     return res.status(401).json({ message: "Invalid token" });
   }
 };
+
+// Register
+router.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "User already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user = new User({ username, email, password: hashedPassword });
+    await user.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.json({ token, user: { id: user._id, username, email } });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
 export default authMiddleware;
